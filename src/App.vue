d1514b671ec45b221486944fba8ec4262f01aafe
Pietroarmill <pietroarmillotta38@gmail.com>
2022-05-24 11:45:10 +0200

    add serie tv

--- src/App.vue
+++ src/App.vue
@@ -28,17 +28,33 @@ export default {
       this.movies = [];
       this.wordSearch = word.toLowerCase();
       console.log(this.wordSearch);
+
       axios.get("https://api.themoviedb.org/3/search/movie", {
         params: {
           api_key: "3879e014591b98424ebc389a9ced879e",
           query: this.wordSearch,
         }
       })
-        .then((resp) => {
-          resp.data.results.forEach(element => {
-            this.movies.push(element);
-          });
-        })
+      .then((resp) => {
+        resp.data.results.forEach(element => {
+          this.movies.push(element);
+        });
+      })
+
+      axios.get("https://api.themoviedb.org/3/search/tv", {
+        params: {
+          api_key: "3879e014591b98424ebc389a9ced879e",
+          query: this.wordSearch,
+        }
+      })
+      .then((resp) => {
+        resp.data.results.forEach(element => {
+
+
+          this.movies.push(element);
+        });
+      })
+
       console.log(this.movies);
       this.wordSearch = '';
     },
@@ -48,4 +64,8 @@ export default {
 
 <style lang="scss">
 @import "./style/common.scss";
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,24 +2,71 @@
   <main class="main">
     <div class="container ">
       <div class="main__card" v-for="item in movieArray" :key="item.id">
-        <div class="image-box" style=" background-image: url('https://image.tmdb.org/t/p/w500/6aNKD81RHR1DqUUa8kOZ1TBY1Lp.jpg');">
+        <div class="image-box">
+          <img class="poster" :src="`https://image.tmdb.org/t/p/w342${item.poster_path}`" alt="">
           <div class="text-box">
-            <p class="title">
-              <span>Titolo:</span>
-              {{ item.title }}
-            </p>
-            <p class="original-title">
-              <span>Titolo originale:</span>
-              {{ item.original_title }}
-            </p>
-            <p class="vote">
-              <span>Voto:</span>
-              {{ item.vote_average / 2 }}
-            </p>
-            <p class="overwiew">
-              <span>Descrizione:</span>
-              {{ item.overview }}
-            </p>
+            <div class="title">
+              <span>Titolo: </span>
+              <p>{{ item.title || item.name }}</p>
+            </div>
+            <div class="original-title">
+              <span>Titolo originale: </span>
+              <p>{{ item.original_title || item.original_name }}</p>
+            </div>
+            <div class="language">
+              <span>Lingua: </span>
+              <!-- <img src="../assets/img/en.jpg" alt=""> -->
+              <img v-if="item.original_language === 'en'" src="../assets/img/en.jpg" alt="">
+              <img v-else-if="item.original_language === 'it'" src="../assets/img/it.jpg" alt="">
+              <img v-else-if="item.original_language === 'fr'" src="../assets/img/Flag_of_France.png" alt="">
+              <img v-else-if="item.original_language === 'sv'" src="../assets/img/sweden.jpg" alt="">
+              <span v-else>{{ item.original_language }}</span>
+            </div>
+            <div class="vote">
+              <span>Voto: </span>
+              <small v-if="item.vote_average / 2 < 0.5">
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+              </small>
+              <small v-else-if="item.vote_average / 2 < 1.5">
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+              </small>
+              <small v-else-if="item.vote_average / 2 < 2.5">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+              </small>
+              <small v-else-if="item.vote_average / 2 < 3.5">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+              </small>
+              <small v-else-if="item.vote_average / 2 < 4.5">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+              </small>
+              <small v-else>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </small>
+            </div>
           </div>
         </div>
       </div>
@@ -30,13 +77,16 @@
 <script>
 export default {
   name: "AppMain",
-  props: ['movieArray'],
-}
+  props: {
+    movieArray: Array,
+  }
+};
 </script>
 
 
 <style lang="scss" scoped>
 @import "../style/common.scss";
+@import '~@fortawesome/fontawesome-free/css/all.min.css';
 
 .main {
   background-color: rgb(173, 230, 206);
@@ -50,23 +100,54 @@ export default {
   }
 
   &__card {
-    width: calc(100% / 3 - 10px);
-    min-height: 400px;
+    width: calc(100% / 4 - 10px);
     margin: 1rem 5px;
-    background-color: rgb(255, 249, 250);
-  }
+    max-width: 342px;
+    background-color: #000000;
 
-  .image-box {
-    height: 100%;
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    .text-box {
-      padding: 10px;
-      span {
-        font-weight: 600;
+    .image-box {
+      .poster {
+        display: block;
+      }
+      
+      .text-box {
+        color: white;
+        padding: .8rem;
+        display: none;
+
+        div {
+          margin: .5rem 0;
+        }
+
+        span {
+          font-weight: 600;
+        }
+
+        p {
+          display: inline-block;
+        }
+
+        img {
+          width: 1rem;
+          height: 1rem;
+          border-radius: 50%;
+          vertical-align: middle;
+        }
+
+        .vote small {
+          color: yellow;
+        }
       }
     }
   }
+}
+
+// Hover
+.main__card:hover .poster {
+  display: none;
+}
+
+.main__card:hover .text-box {
+  display: block;
 }
 </style>
